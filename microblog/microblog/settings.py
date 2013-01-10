923f3253aed2ce46b7d3e5f4e4a049abe0c54f6d
Ignacio Rodrigo <irodrigo17@gmail.com>
2013-01-09 22:54:12 -0200

    Start microblog_app app

--- microblog/microblog/settings.py
+++ microblog/microblog/settings.py
@@ -116,6 +116,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'south',
+    'microblog_app',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
