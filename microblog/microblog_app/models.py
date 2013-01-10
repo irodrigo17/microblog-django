0d828951d61c972060acd99cd8d366ba3fecfeaa
Ignacio Rodrigo <irodrigo17@gmail.com>
2013-01-10 00:10:29 -0200

    Add relation between User and Post

--- microblog/microblog_app/models.py
+++ microblog/microblog_app/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
 	text = models.CharField(max_length=200)
 	created_at = models.DateTimeField("date created")
+	user = models.ForeignKey(User)
 
 	def __unicode__(self):
 		return self.text
