2fd23ce87916144c044f960900b7d7bd0f0705e6
Ignacio Rodrigo <irodrigo17@gmail.com>
2013-01-10 00:50:16 -0200

    Improved API

--- microblog/microblog/urls.py
+++ microblog/microblog/urls.py
@@ -1,13 +1,17 @@
 from django.conf.urls import *
+from tastypie.api import Api
 from microblog_app.api import PostResource, UserResource
-
-
 from django.contrib import admin
+
+# Init admin
 admin.autodiscover()
 
-post_resource = PostResource()
-user_resource = UserResource()
+# Init tastypie
+v1_api = Api(api_name='v1')
+v1_api.register(UserResource())
+v1_api.register(PostResource())
 
+# define URL patterns
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'microblog.views.home', name='home'),
@@ -17,6 +21,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/', include(post_resource.urls)),
-    url(r'^api/', include(user_resource.urls)),
+    url(r'^api/', include(v1_api.urls)),
 )
--- microblog/microblog_app/api.py
+++ microblog/microblog_app/api.py
@@ -1,13 +1,19 @@
 from tastypie.resources import ModelResource
 from microblog_app.models import Post
 from django.contrib.auth.models import User
-
-class PostResource(ModelResource):
-	class Meta:
-		queryset = Post.objects.all()
-		resource_name = 'post'
+from tastypie import fields
+from tastypie.authorization import Authorization
 
 class UserResource(ModelResource):
 	class Meta:
 		queryset = User.objects.all()
 		resource_name = 'user'
+		fields = ['username', 'first_name', 'last_name', 'last_login']
+
+class PostResource(ModelResource):
+	user = fields.ForeignKey(UserResource, 'user')
+
+	class Meta:
+		queryset = Post.objects.all()
+		resource_name = 'post'
+		authorization = Authorization()
