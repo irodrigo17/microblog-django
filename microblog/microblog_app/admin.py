22d87e3127a78a3154ec864857c1443352ed84ac
Ignacio Rodrigo <irodrigo17@gmail.com>
2013-01-20 22:31:32 -0200

    Register microblog_app.models.User with django admin.

--- microblog/microblog_app/admin.py
+++ microblog/microblog_app/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from microblog_app.models import Post, User, Follow, Like, Share
 from tastypie.models import ApiKey, ApiAccess
 
+admin.site.register(User)
 admin.site.register(Post)
 admin.site.register(Follow)
 admin.site.register(Like)
